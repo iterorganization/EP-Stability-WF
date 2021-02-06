88f1b54cc11a2c8cf86d639823c4be724c6b158a
Popa Alin <popaa@hpc-login02.iter.org>
2021-02-06 13:18:50 +0100

    Added finder into the README and the proper paths for hagis2/finder

--- README.md
+++ README.md
@@ -44,9 +44,19 @@ $ gmake hagis1_wf OPTS=IMAS,WF
 6. Compile and run FC2K for HAGIS 2:
 ```bash
 $ cd src/hagis2
-$ gmake hagis2_wf OPTS=MPI,IMAS,WF
+$ gmake hagis2_WF OPTS=MPI,IMAS,WF
 ```
-7. After the creation of the actors you should be able to go to **EP Stabiliy WF** and run:
+7. Compile and run FC2K for finder_MPI:
+```bash
+$ cd ligka/finder/src
+$ gmake finder9_wf OPTS=IMAS,MPI,WF
+```
+8. Compile and run FC2K for finder_serial:
+```bash
+$ cd ligka/finder/src
+$ gmake finder9_wf_s OPTS=IMAS,WF
+```
+9. After the creation of the actors you should be able to go to **EP Stabiliy WF** and run:
 ```bash
 $ python gui_HL.py
 ```
--- module_start_python.sh
+++ module_start_python.sh
@@ -18,6 +18,7 @@ module load SPRNG/2.0b-intel-2018a
 # CREATE FOLDER FOR ACTOR POOL
 export ACTOR_FOLDER_WF=~/public/imas_actors #edit this line to best suit your needs
 export ACTOR_FOLDER=public/imas_actors #edit this line to best suit your needs
+export HAGIS2PATH=$ACTOR_FOLDER/hagis2/hagis2/native_wrapper/lib/def  # DO NOT CHANGE THIS LINE! (required by the finder)
 mkdir -p ~/$ACTOR_FOLDER
 
 # EXTEND PYTHON PATH AND AVOID DOUBLONS (where the actors are under the form: "/actor_name/version(if any)"
@@ -25,5 +26,7 @@ export PYTHONPATH=$ACTOR_FOLDER_WF/hagis1:$PYTHONPATH
 export PYTHONPATH=$ACTOR_FOLDER_WF/hagis2:$PYTHONPATH
 export PYTHONPATH=$ACTOR_FOLDER_WF/helena_imas:$PYTHONPATH
 export PYTHONPATH=$ACTOR_FOLDER_WF/ligka:$PYTHONPATH
+export PYTHONPATH=$ACTOR_FOLDER_WF/finder9:$PYTHONPATH
+export PYTHONPATH=$ACTOR_FOLDER_WF/finder9_s:$PYTHONPATH
 export PYTHONPATH=$ACTOR_FOLDER_WF/chease:$PYTHONPATH
 export PYTHONPATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PYTHONPATH}))')"
