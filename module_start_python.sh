97b390eb9f0d05f89d6ddc6fea301a55fb9669ca
Popa Alin <popaa@hpc-login02.iter.org>
2021-04-20 09:29:49 +0200

    Added new FC2K version to match IMAS

--- module_start_python.sh
+++ module_start_python.sh
@@ -7,7 +7,7 @@ module load MUMPS/5.1.2-intel-2018a-metis/
 module load NAG/26-intel-2018a
 module load SLATEC/4.1-iccifort-2018.1.163-GCC-6.4.0-2.28
 module load ppplib
-module load FC2K/4.13.2-Java-1.8
+module load FC2K/4.13.5-Java-1.8
 module load XMLlib/3.3.1-intel-2018a
 module load lxml/4.2.0-intel-2018a-Python-3.6.4
 module load sh/1.12.14-intel-2018a-Python-3.6.4
