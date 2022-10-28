5428b6f790a06d7f6fb63853b108fe8d1e890126
Thomas Hayward-Schneider <thomas.hayward@ipp.mpg.de>
2022-10-28 20:01:39 +0200

    Tool for tagging (also move run_all_ci.sh to shell subfolder)

---
+++ tools/shell/make_release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -v
+
+if [ $# -lt 1 ]; then
+    echo "At least 1 argument required. Usage:"
+    echo "$0 tagname [commit message]"
+    exit 1
+fi
+if [ $# -gt 1 ]; then
+    USE_MESSAGE=1
+fi
+
+VER=$1
+shift
+
+if [ $USE_MESSAGE -eq 1 ]; then
+    MESSAGE="$@"
+fi
+
+sed -i -r "s/(version[ ]*=[ ]*).*$/\1\"${VER}\"/" setup.py
+git add setup.py
+
+if [ ${USE_MESSAGE} -eq 1 ]; then
+    git commit -m "${VER}: ${MESSAGE}"
+    git tag -a ${VER} -m "${MESSAGE}"
+else
+    git tag -a ${VER}
+    git commit -m "${VER} release"
+fi
+
+echo "Now please check, and then: git push origin develop; git push origin ${VER}"
