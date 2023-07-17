6703237acf613e7c2b0e249f9adbd370a635e5ca
Thomas Hayward-Schneider <thomas.hayward@ipp.mpg.de>
2023-07-17 14:48:41 +0200

    Fix tagging script

--- tools/shell/make_release.sh
+++ tools/shell/make_release.sh
@@ -7,6 +7,7 @@ if [ $# -lt 1 ]; then
     echo "$0 tagname [commit message]"
     exit 1
 fi
+USE_MESSAGE=0
 if [ $# -gt 1 ]; then
     USE_MESSAGE=1
 fi
@@ -25,8 +26,8 @@ if [ ${USE_MESSAGE} -eq 1 ]; then
     git commit -m "${VER}: ${MESSAGE}"
     git tag -a ${VER} -m "${MESSAGE}"
 else
-    git tag -a ${VER}
     git commit -m "${VER} release"
+    git tag -a ${VER}
 fi
 
 echo "Now please check, and then: git push origin develop; git push origin ${VER}"
