71175f986a80c41d6b186107b14195e495df8761
Alin Popa <alin.popa@ipp.mpg.de>
2023-07-25 16:10:36 +0200

    Wrote message if no ids is found with a certain occurrence

--- ep_stability_wf/workflow/workflow_components.py
+++ ep_stability_wf/workflow/workflow_components.py
@@ -116,14 +116,15 @@ def actor_call(actor_name, config_folder_path, system_params, species_input, sce
                 equilibrium_in = input.get_slice(
                     ids_name, time[itime], imas.imasdef.PREVIOUS_SAMPLE, occurrence=ids_occ)
                 equilibrium_occ = ids_occ
+                
             if ids_name == 'mhd_linear':
                 try:
                     mhd_linear_in = input.get_slice(
                         ids_name, time[itime], imas.imasdef.PREVIOUS_SAMPLE, occurrence=ids_occ)
                     mhd_linear_occ = ids_occ
                 except:
-                    mhd_linear_in = mhd_linear_in
-                    mhd_linear_occ = ids_occ
+                    print(f'No mhd_linear with occurrence {mhd_linear_occ} found, using empty mhd_linear')
+                    pass
 
             if ids_name == 'distributions':
                 if ids_occ == 0:
@@ -131,13 +132,16 @@ def actor_call(actor_name, config_folder_path, system_params, species_input, sce
                         distributions_in_1 = input.get_slice(
                             ids_name, time[itime], imas.imasdef.PREVIOUS_SAMPLE, occurrence=ids_occ)
                     except:
-                        distributions_in_1 = distributions_in_1
+                        print(f'No distributions with occurrence {distributions_occ_1} found, using empty distributions')
+                        pass
                 elif ids_occ == 1:
                     try:
                         distributions_in_2 = input.get_slice(
                             ids_name, time[itime], imas.imasdef.PREVIOUS_SAMPLE, occurrence=ids_occ)
                     except:
-                        distributions_in_2 = distributions_in_2
+                        print(f'No distributions with occurrence {distributions_occ_2} found, using empty distributions')
+                        pass
+                    
             if ids_name == 'core_profiles':
                 core_profiles_in = input.get_slice(
                     ids_name, time[itime], imas.imasdef.PREVIOUS_SAMPLE, occurrence=ids_occ)
