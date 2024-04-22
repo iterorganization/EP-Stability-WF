ebc1be1606f1c9d971b52a1540831b63bb1cf8a6
Alin Popa <alin.popa@ipp.mpg.de>
2024-04-22 14:58:40 +0200

    Fixed generation of any xml params

--- ep_stability_wf/interface/create_workflow_param.py
+++ ep_stability_wf/interface/create_workflow_param.py
@@ -4,25 +4,15 @@ from lxml import etree
 def create_workflow_param_from_file(filepath):
     tree = etree.parse(filepath)
     root = tree.getroot()
-
-    name0 = root[0].attrib["display"]
-    name1 = root[1].attrib["display"]
-    name2 = root[2].attrib["display"]
-
     workflow_param = {}
-    workflow_param = {name0: {}, name1: {}, name2: {}}
-
-    for elem in root[0].iter():
-        if len(elem) == 0 and elem.tag is not etree.Comment:
-            workflow_param[name0][elem.tag] = (elem.text, elem.attrib["display"])
-
-    for elem in root[1].iter():
-        if len(elem) == 0 and elem.tag is not etree.Comment:
-            workflow_param[name1][elem.tag] = (elem.text, elem.attrib["display"])
-
-    for elem in root[2].iter():
-        if len(elem) == 0 and elem.tag is not etree.Comment:
-            workflow_param[name2][elem.tag] = (elem.text, elem.attrib["display"])
+    for level in root:
+        name = level.attrib["display"]
+        if name not in workflow_param:
+            temp_dict = {}
+        for elem in level.iter():
+            if len(elem) == 0 and elem.tag is not etree.Comment:
+                temp_dict[elem.tag] = (elem.text, elem.attrib["display"])
+        workflow_param[name] = temp_dict
 
     return workflow_param
 
