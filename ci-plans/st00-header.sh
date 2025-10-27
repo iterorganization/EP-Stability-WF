e78fd61319d7c1a858b361a1196ead649bbeb773
Thomas Hayward-Schneider <thomas.hayward@ipp.mpg.de>
2025-10-27 13:40:13 +0100

    Update versions in CI modules

--- ci-plans/st00-header.sh
+++ ci-plans/st00-header.sh
@@ -13,8 +13,8 @@ lxml/4.9.3-GCCcore-13.2.0 \
 Tkinter/3.11.5-GCCcore-13.2.0 \
 PyYAML/6.0.1-GCCcore-13.2.0 \
 HELENA/2.1.0-intel-2023b-DD-3.42.0 \
-LIGKA/2.1.0-intel-2023b-DD-3.42.0 \
-CHEASE/14.5-intel-2023b-DD-3.42.0
+LIGKA/2.1.2-intel-2023b-DD-3.42.0 \
+CHEASE/14.6.3-intel-2023b-DD-3.42.0
 
 module list
 
